0ab177a6d021c25b570073bb969a714695f0aac0
Eugen <hellvinc@gmail.com>
2020-12-04 16:07:20 +0200

    test commit 2

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -49,6 +49,7 @@ class SiteController extends Controller
      */
     public function actions()
     {
+
         return [
             'error' => [
                 'class' => 'yii\web\ErrorAction',
